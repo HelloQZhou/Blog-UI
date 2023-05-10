befb5cc3941c1fcd5ebc8aae92a03181e889c4c1
QZone <2024096103@qq.com>
2023-05-10 11:01:11 +0800

    博客前台

---
+++ sg-vue-admin/src/views/dashboard/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      博客管理系统
+    </div>
+    <div>
+      所用技术：
+      <br/>
+      后端：SpringBoot、MybatisPlus、Mysql、Redis、SpringSecurity、EasyExcel、Swagger2...
+    </div>
+    <div>
+      前端：Vue、ElementUI...
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
